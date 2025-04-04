ac0f75efdc6e1dc88c8afd34889bc8c05144426c
Aluno <thayna.matos.carlos@gmail.com>
2025-04-04 08:35:55 -0300

    Atualizações

--- js/scripts.js
+++ js/scripts.js
@@ -53,3 +53,68 @@ class contaBancaria {
 const conta = new contaBancaria(1000);
 conta.depositar(1000);
 console.log(conta.getSaldo());
+
+/* --------------------------------------------------------------------------------- */
+
+class Pessoa {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  //MÉTODOS - SAO FUNCOES DENTRO DE UMA CLASSE, SERVEM PARA EXECUTAR AÇOES OU MANIPULAR OS ATRIBUTOS!
+  apresentacao() {
+    return `Olá meu nome é ${this.nome}, tenho ${this.idade} anos!`;
+  }
+}
+
+const minhaApresentacao = new Pessoa("Thayna", 17);
+console.log(minhaApresentacao.apresentacao());
+
+/* --------------------------------------------------------------------------------- */
+
+class Steam {
+  #saldo;
+
+  constructor(saldoInicialSteam) {
+    this.#saldo = saldoInicialSteam;
+  }
+
+  depositarValorSteam(valor) {
+    if (valor > 0) {
+      this.#saldo += valor;
+    } else {
+      console.log("O depósito é invalido");
+    }
+  }
+  getSaldoSteam() {
+    return `Seu saldo na Steam é de ${this.#saldo}`;
+  }
+}
+
+const steam = new Steam(1000);
+steam.depositarValorSteam(300);
+console.log(steam.getSaldoSteam());
+
+/* --------------------------------------------------------------------------------- */
+
+class usuario {
+  constructor(senha) {
+    this.senha = senha;
+  }
+
+  #verificarForcadaSenha(senha) {
+    return senha.length > 6 ? true : false;
+  }
+
+  criarConta(senha) {
+    if (this.#verificarForcadaSenha(this.senha)) {
+      return console.log("Conta criada!!!");
+    }
+
+    return console.log("Senha Inválida!!!");
+  }
+}
+
+const user = new usuario("123456789");
+user.criarConta();
